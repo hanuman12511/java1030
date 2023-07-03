2ef083cdab1999bc7eeca6185d1e54b5f2e4dcca
hanumansaini12511 <er.hanumansaini@gmail.com>
2023-07-03 10:34:08 +0530

    inherit

---
+++ Inheri.java
@@ -0,0 +1,51 @@
+abstract class DataInfo{
+    void display(){
+        System.out.println("info");
+
+    }
+    abstract void show_Info();
+}
+class InfoTeam{
+    private int id;
+    private String name;
+    void setId(int id){
+        this.id =id;
+    }
+    void setName(String name){
+        this.name =name;
+    }
+
+    int getId(){
+        return id;
+    }
+    
+    String getName(){
+        return name;
+    }
+}
+class Emp_Team1 extends InfoTeam
+{
+    void show_Info(){
+        setId(10);
+        setName("emp1");
+        System.out.println(getId());
+        System.out.println(getName());
+    }
+}
+class Emp_Team2 extends InfoTeam
+{
+    void show_Info(){
+         setId(11);
+        setName("emp2");
+        System.out.println(getId());
+        System.out.println(getName());
+    }
+}
+public class Inheri {
+    public static void main(String...rr){
+        Emp_Team1 et1=new Emp_Team1();
+        et1.show_Info();
+        Emp_Team2 et2=new Emp_Team2();
+        et2.show_Info();
+    }
+}
